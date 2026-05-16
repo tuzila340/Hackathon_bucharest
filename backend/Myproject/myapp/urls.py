68b6ee4e331cd691725c625f6b2b58aa78f6250b
oleksiibiloys <beloys0404@gmail.com>
2026-05-16 10:53:04 +0300

    commit all

--- backend/Myproject/myapp/urls.py
+++ backend/Myproject/myapp/urls.py
@@ -1,20 +1,28 @@
-from . import views
 from django.urls import path
 from rest_framework.authtoken import views as auth_views
+from . import views
+
+
+
+
 
 urlpatterns = [
-    #запити
-    path('requests/', views.HelpRequestListAPI.as_view(), name='api_list'),
-    path('requests/<int:pk>/', views.HelpRequestDetailAPI.as_view(), name='api_detail'),
+    # запити
+    path('api/requests/', views.HelpRequestListAPI.as_view(), name='api_list'),
+    path('api/requests/<int:pk>/', views.HelpRequestDetailAPI.as_view(), name='api_detail'),
 
     # користувачі
-    path('register/', views.RegisterView.as_view(), name='api_register'),
-    path('profile/', views.ProfileUpdateAPI.as_view(), name='api_profile'),
-    path('login/', auth_views.obtain_auth_token, name='api_login'),
-    path('requests/<int:pk>/accept/', views.AcceptHelpRequestAPI.as_view(), name='api_acept_request'),
-    path('requests/<int:pk>/complete/', views.CompleteHelpRequestAPI.as_view(), name='api_complete_request'),
-    path('my-requests/', views.MyHelpRequestsAPI.as_view(), name='my_requests'),
-    path('my-tasks/', views.MyVolunteerTasksAPI.as_view(), name='my_tasks'),
-    path('requests/<int:pk>/favorite/', views.ToggleFavoriteAPI.as_view(), name='api_toggle_favorite'),
-    path('my-favorites/', views.MyFavoritesAPI.as_view(), name='my_favorites'),
+    path('api/register/', views.RegisterView.as_view(), name='api_register'),
+    path('api/profile/', views.ProfileUpdateAPI.as_view(), name='api_profile'),
+    path('api/login/', auth_views.obtain_auth_token, name='api_login'),  # Тепер збігається з фронтом!
+
+    # дії волонтера
+    path('api/requests/<int:pk>/accept/', views.AcceptHelpRequestAPI.as_view(), name='api_acept_request'),
+    path('api/requests/<int:pk>/complete/', views.CompleteHelpRequestAPI.as_view(), name='api_complete_request'),
+
+    # особистий кабінет
+    path('api/my-requests/', views.MyHelpRequestsAPI.as_view(), name='my_requests'),
+    path('api/my-tasks/', views.MyVolunteerTasksAPI.as_view(), name='my_tasks'),
+    path('api/requests/<int:pk>/favorite/', views.ToggleFavoriteAPI.as_view(), name='api_toggle_favorite'),
+    path('api/my-favorites/', views.MyFavoritesAPI.as_view(), name='my_favorites'),
 ]
