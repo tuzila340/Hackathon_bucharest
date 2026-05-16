b4f51a28df57b2882aefec505984664f6e8719fb
tuzila <221207985+tuzila340@users.noreply.github.com>
2026-05-16 09:05:01 +0300

    s

--- front/src/SeekerList.jsx
+++ front/src/SeekerList.jsx
@@ -7,7 +7,6 @@ import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
 import Paper from '@mui/material/Paper';
 import { useState, useEffect } from "react";
-import AddSeekerDialog from "./AddSeekerDialog";
 
 function SeekerList() {
   const [seekers, setSeekers] = useState([]);
